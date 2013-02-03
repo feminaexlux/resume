06de0bc560dc52449b6087426cac05e3cea04b37
Lani Aung <delish@flavoredorange.net>
2013-02-03 15:18:50 -0800

    Finishing education & combining and minifying css

--- index.php
+++ index.php
@@ -16,16 +16,13 @@
 		<meta name="keywords" content="java,web,developer,resume" />
 		<meta name="author" content="Lani Aung">
 
-		<link href="http://fonts.googleapis.com/css?family=Amethysta|Fjalla+One" rel="stylesheet" type="text/css">
-		<link href="stylesheets/reset.css" rel="stylesheet" type="text/css">
-		<link href="stylesheets/font-awesome.min.css" rel="stylesheet">
 		<link href="http://microformats.org/profile/hcard" rel="profile">
+		<link href="http://fonts.googleapis.com/css?family=Amethysta|Fjalla+One" rel="stylesheet" type="text/css">
+		<link href="stylesheets/combined.min.css" rel="stylesheet" type="text/css">
 		<!--[if IE 7]>
-		<link rel="stylesheet" href="stylesheets/font-awesome-ie7.min.css">
+			<link rel="stylesheet" href="stylesheets/font-awesome-ie7.min.css">
 		<![endif]-->
-		<link href="stylesheets/screen.css" rel="stylesheet" type="text/css">
-		<link href="stylesheets/print.css" rel="stylesheet" type="text/css" media="print">
-		<link href="stylesheets/media.css" rel="stylesheet" type="text/css">
+		<link href="stylesheets/print.min.css" rel="stylesheet" type="text/css" media="print">
 	</head>
 	<body>
 		<article id="resume">
@@ -72,20 +69,33 @@
 				<div class="content">
 					<ul>
 						<!-- LINKS AHOY -->
-						<li><?php echo $html; ?> years of experience with HTML programming, and LAMP and WAMP configurations.</li>
-						<li><?php echo $linux; ?> years of active use with the Linux/UNIX operating system environments, such as <a href="https://www.redhat.com/">Red Hat</a>, <a href="http://www.ubuntu.com/">Ubuntu</a>, <a href="http://www.debian.org/">Debian</a>, and <a href="https://www.archlinux.org/" title="My current favorite">Arch</a>.</li>
-						<li><?php echo $mysql_php; ?> years experience with <a href="https://www.mysql.com/">MySQL</a>, <a href="http://www.php.net/">PHP</a>, <a href="http://jquery.com/">jQuery</a>, <a href="http://www.python.org/">Python</a> and <a href="http://www.java.com/en/">Java</a> using development environments such as <a href="http://www.eclipse.org/">Eclipse</a>, <a href="http://www.sublimetext.com/" title="This resume was created using Sublime">Sublime Text</a> and <a href="http://www.jetbrains.com/idea/" title="So far the most complex and feature filled IDE I've seen">IntelliJ IDEA</a>.</li>
+						<li><?php echo $html; ?> years of experience with HTML programming, and <abbr title="Linux, Apache (httpd), MySQL, PHP">LAMP</abbr> and <abbr title="Windows, Apache (httpd), MySQL, PHP">WAMP</abbr> configurations.</li>
+						<ul>
+							<li><?php echo $linux; ?> years of active use with the Linux/UNIX operating system environments, such as <a href="https://www.redhat.com/">Red Hat</a>, <a href="http://www.ubuntu.com/">Ubuntu</a>, <a href="http://www.debian.org/">Debian</a>, and <a href="https://www.archlinux.org/" title="My current favorite">Arch</a>.</li>
+							<li><?php echo $mysql_php; ?> years experience with <a href="https://www.mysql.com/">MySQL</a>, <a href="http://www.php.net/">PHP</a>, <a href="http://jquery.com/">jQuery</a>, <a href="http://www.python.org/">Python</a> and <a href="http://www.java.com/en/">Java</a> using development environments such as <a href="http://www.eclipse.org/">Eclipse</a>, <a href="http://www.sublimetext.com/" title="This resume was created using Sublime">Sublime Text</a> and <a href="http://www.jetbrains.com/idea/" title="So far the most complex and feature filled IDE I've seen">IntelliJ IDEA</a>.</li>
+						</ul>
 						<li><?php echo $struts_hibernate_spring; ?> years of experience with <a href="https://struts.apache.org/2.x/index.html">Struts 2</a>, <a href="http://tomcat.apache.org/index.html">Tomcat</a>, <a href="http://www.hibernate.org/">Hibernate</a>, <a href="http://www.springsource.org/">Spring</a>, <a href="https://maven.apache.org/">Maven</a>, SOAP/WSDL, <a href="http://subversion.apache.org/">SVN</a>/<a href="http://git-scm.com/" title="Not the easiest, but this seems like a clear winner">GIT</a>, <span title="Site is down...">JUnit</span>, <a href="https://code.google.com/p/mockito/">Mockito</a>, and <a href="https://code.google.com/p/powermock/" title="Definitely introduces a performance hit for testing">PowerMock</a>.</li>
 						<li><?php echo $play_ruby_boostrap; ?> year of experience with <a href="http://www.playframework.org/">Play 2.0 Framework</a>, <a href="http://www.lighttpd.net/">Lighttpd</a>, <a href="http://www.ruby-lang.org/en/">Ruby</a>, <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a> and <a href="http://fortawesome.github.com/Font-Awesome/">FontAwesome</a>.</li>
 						<li><?php echo $rabbitmq; ?> year of experience with <a href="http://www.rabbitmq.com/">RabbitMQ</a> AMQP implementation.</li>
 						<li>Active utilization of virtualization technologies like <a href="http://www.vmware.com/">VMWare</a>, <a href="https://www.virtualbox.org/">Virtualbox</a> and <a href="https://www.microsoft.com/windows/virtual-pc/">Virtual PC</a>.</li>
 						<li>Extensive experience with <a href="http://www.adobe.com/products/creativesuite.html" title="Student discount came in incredibly helpful">Adobe products</a>, such as Photoshop, Illustrator, Acrobat, Dreamweaver and Flash.</li>
 						<li>Extensive use of third party software/plugins like <a href="http://www.webyog.com/">SQLYog</a>, <a href="http://www.heidisql.com/">HeidiSQL</a>, <a href="http://www.quest.com/toad-for-mysql/">Toad</a>, <a href="http://www.terracotta.org/">Terracotta</a>, <a href="http://www.splunk.com/" title="Best indexer for logs">Splunk</a>, <a href="http://newrelic.com" title="Nice statistics visualizations">New Relic</a>, <a href="http://jenkins-ci.org/">Jenkins CI</a>, <a href="https://code.google.com/p/gerrit/">Gerrit</a>, and <a href="https://www.atlassian.com/" title="These are very good products!">Atlassian products</a> like JIRA, Confluence, Bamboo and Fisheye.</li>
-						<li>Highly adaptable, quick to learn new methods and implementations.</li>
-						<li>Motivated to work in teams or alone in projects with defined goals.</li>
-						<li>Communicative, able to point out issues, explain ideas and concepts clearly, and suggest new ideas for improvement.</li>
-						<li>Designs with independent-platform, cross-browser and accessibility standards.</li>
 					</ul>
+
+					<hr>
+
+					<p>
+						Highly adaptable, quick to learn new methods and implementations.
+					</p>
+					<p>
+						Motivated to work in teams or alone in projects with defined goals.
+					</p>
+					<p>
+						Communicative, able to point out issues, explain ideas and concepts clearly, and suggest new ideas for improvement.
+					</p>
+					<p>
+						Designs with independent-platform, cross-browser and accessibility standards.
+					</p>
 				</div>
 			</section>
 
@@ -119,7 +129,7 @@
 
 					<dl>
 						<time datetime="2008-12-01">December 2008 – September 2009</time>
-						<dt><strong>Student Programmer</strong> at <a href="http://www.picsauditing.com/">Research Computing Support, UC Irvine</a></dt>
+						<dt><strong>Student Programmer</strong> at <a href="http://oit.uci.edu/">Research Computing Support, UC Irvine</a></dt>
 						<dd>
 							<section class="summary">
 								<p>
@@ -133,6 +143,8 @@
 					</dl>
 				</div>
 			</section>
+
+			<hr>
 
 			<section class="education">
 				<header>
@@ -141,50 +153,25 @@
 				
 				<div class="content">
 					<dl>
-						<time datetime="2009-08-10">Summery 2009</time>
-						<dt>Graduated from <a href="http://www.uci.edu/">University of California, Irvine</a>.</dt>
+						<time datetime="2009-08-10">Summer 2009</time>
+						<dt>Graduated from <a href="http://www.uci.edu/" title="Zot zot">University of California, Irvine</a>.</dt>
 						<dd>
 							<section class="summary">
 								<p>
-									Notable achievements include leading multiple projects that combined became a whole new line of service employing user level data (expanded from company level data), attracting new customers and broadening the client base. Implemented new features that provided major sources of revenue, such as allowing for pre-registration, expediting the customer-to-company relationship establishment. Became team leader and release manager, leading code reviews, training, paired programming, and giving presentations on technologies.
+									Bachelor of Science degree achieved in Biological Sciences, with a minor in Information and Computer Science.
 								</p>
 								<p>
-									Internationalized a once US-only site application to reach global audiences, using home grown technology that was easily exportable and importable for ease of translation.
-								</p>
-								<p>
-									Experienced with communicating effectively and directly with clients and staff members throughout all departments, providing flexibility when requirements change, delivering on time, robust products focusing on 100% stability and satisfaction. Several projects were given praise and acclaim from senior level management, personally became the go-to woman for in depth explanations and quick assessments.
-								</p>
-							</section>
-						</dd>
-					</dl>
-
-					<hr>
-
-					<dl>
-						<time datetime="2008-12-01">December 2008 – September 2009</time>
-						<dt><strong>Student Programmer</strong> at <a href="http://www.picsauditing.com/">Research Computing Support, UC Irvine</a></dt>
-						<dd>
-							<section class="summary">
-								<p>
-									Responsible for the creation of an interactive and dynamic online system to manage data center information. Functions include dynamic table generation, data storage into MySQL databases, searches, nightly PDF/image generation, and user management.
-								</p>
-								<p>
-									Information  and  Academic  Technologies  of  UCI  will  use  this  highly  regarded system to maintain up to date information about data centers within UCI, keeping track of server locations and server statuses, unifying geographic information with detailed component properties.
+									GPA of 3.85 in Information and Computer Science minor.
 								</p>
 							</section>
 						</dd>
 					</dl>
 				</div>
-
-				EDUCATION Summer 2009 graduate of University of California, Irvine.
-Bachelor of Science degree achieved in Biological Sciences, with a minor in
-Information and Computer Science.
-GPA of 3.85 in Information and Computer Science minor.
 			</section>
 			
 			<footer>
 				<hr>
-				<p>Last updated 2013-01-28</p>
+				<p>Last updated 2013-02-03</p>
 				<p>&copy; <span title="aka Ulanda S. Aung"><a href="mailto:uaung@alumni.uci.edu">Lani Aung</a></span></p>
 			</footer>
 		</article>
